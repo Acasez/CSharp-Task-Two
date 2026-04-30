eb214222e600ef3d88ed1c93dce19b69f7d898ed
Acasez <edvin.sanne@gmail.com>
2026-04-30 13:36:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // Flytta hit din top level kod om du vill köra med Main istället
+    }
+}
