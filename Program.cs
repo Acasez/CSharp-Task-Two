6ad55cd7c12df44775214e367f77bfc03826f155
Acasez <edvin.sanne@gmail.com>
2026-04-30 14:05:30 +0200

    Fixed movie goers part

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Program
 {
@@ -10,6 +11,8 @@ public class Program
         while (looping)
         {
             Console.WriteLine("------");
+            Console.WriteLine("Press 0 to exit program");
+            Console.WriteLine("Press 1 to get movie ticket price");
             Console.WriteLine("Enter input:");
             string enteredInput = Console.ReadLine();
             int input = 0;
@@ -20,10 +23,56 @@ public class Program
                     Console.WriteLine("Closing program");
                     looping = false;
                     break;
+                case 1:
+                    Console.WriteLine("Entering movie price realm.");
+                    MovieGoers();
+                    break;
                 default:
                     Console.WriteLine("Invalid Input");
                     break;
             }
+        }
+    }
+
+    public static void MovieGoers()
+    {
+        Console.WriteLine("Enter amount of moviegoers:");
+        string enteredInput = Console.ReadLine();
+        int moviegoers = 0;
+        int.TryParse(enteredInput, out moviegoers);
+        int totalTickerCost = 0;
+        for (int i = 0; i < moviegoers; i++)
+        {
+            totalTickerCost += MoviePrice();
+        }
+        Console.WriteLine(moviegoers + " moviegoers. \nTotal cost is " +  totalTickerCost + "kr");
+    }
+
+    public static int MoviePrice()
+    {
+        int youthPrice = 80;
+        int seniorPrice = 90;
+        int adultPrice = 120;
+
+        Console.WriteLine("Enter age:");
+        string enteredInput = Console.ReadLine();
+        int input = 0;
+        int.TryParse(enteredInput, out input);
+
+        if (input < 20)
+        {
+            Console.WriteLine("Movie price for youths is :: " + youthPrice + "kr");
+            return youthPrice;
+        }
+        else if (input > 64)
+        {
+            Console.WriteLine("Movie price for seniors is :: " + seniorPrice + "kr");
+            return seniorPrice;
+        }
+        else
+        {
+            Console.WriteLine("Movie price for adults is : " + adultPrice + "kr");
+            return adultPrice;
         }
     }
 }
